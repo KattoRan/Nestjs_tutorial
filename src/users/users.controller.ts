72adf26f5fb095f0a9499f8c571182a0a92a2d16
Than Cat Ngoc Lan <chantran29102004@gmail.com>
2025-07-23 12:39:43 +0700

    feat(follow): implement follow, unfollow users endpoints

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,10 +1,21 @@
-import { Controller, Get, UseGuards, Request, Body, Patch } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  UseGuards,
+  Request,
+  Body,
+  Patch,
+  Post,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { UsersService } from './users.service';
 import { ApiBearerAuth } from '@nestjs/swagger';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { User } from '@prisma/client';
 import { GetUser } from 'src/auth/decorator/get-user.decorator';
+import { CurrentUser } from 'src/auth/types/current-user.interface';
 
 @ApiBearerAuth()
 @UseGuards(AuthGuard('jwt'))
@@ -13,13 +24,29 @@ export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Get('profile')
-  async getProfile(@GetUser() user: User) { 
+  async getProfile(@GetUser() user: User) {
     const fullUserInfo = await this.usersService.findById(user.id);
     return fullUserInfo;
   }
-  
+
   @Patch('user')
   updateProfile(@GetUser() user: User, @Body() updateUserDto: UpdateUserDto) {
     return this.usersService.update(user.id, updateUserDto);
+  }
+
+  @Post('profiles/:username/follow')
+  async followUser(
+    @GetUser() currentUser: CurrentUser,
+    @Param('username') username: string,
+  ) {
+    return this.usersService.followUser(currentUser, username);
+  }
+
+  @Delete('profiles/:username/follow')
+  async unfollowUser(
+    @GetUser() currentUser: CurrentUser,
+    @Param('username') username: string,
+  ) {
+    return this.usersService.unfollowUser(currentUser, username);
   }
 }
